4c8cbb99c604c70e17a3a3a577b0754f2c99f075
Alexander <fredi.caballero@unmsm.edu.pe>
2022-11-17 17:19:44 -0500

    ejemplo2

--- index.js
+++ index.js
@@ -1,6 +1,20 @@
-const express = require('express')
-const app = express()
-const PORT = 3001
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+// Conexion a base de datos
+const mongoose = require('mongoose');
+const user = 'demo_4';
+const password = 'xfUMf5nqM7RrIJIB';
+const dbname = 'DBVuelos';
+const uri = `mongodb+srv://${user}:${password}@vuelos.uea1vov.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+
+mongoose.connect(uri,
+  {useNewUrlParser: true, useUnifiedTopology: true}  
+)
+.then(()=>console.log('Base de datos ok'))
+.catch(e=>console.log(e))
+
 
 app.use(express.json())
 
